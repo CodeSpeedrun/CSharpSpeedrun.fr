b7a53f90684fea9863a61253c80c0e2ed53e4235
Code Speedrun <122196247+CodeSpeedrun@users.noreply.github.com>
2024-02-10 14:17:36 +0400

    msg

--- 00_.NET Basics/.NET aliases.cs
+++ 00_.NET Basics/.NET aliases.cs
@@ -1,19 +1,22 @@
 /* .NET aliases
-Since C# applications run on top of the .NET framework, it is possible to use the .NET data type alias instead of the C# data types and the output will be identical.
+Since C# applications run on top of the .NET framework, 
+it is possible to use the .NET data type alias instead of the C# data types and the output will be identical.
 */
- using System;
+using System;
 
-namespace CSharpSpeedrun
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-         int myInt= 45643;
-         System.Int32 mySpecialInt = 45643; 
+namespace CSharpSpeedrun {
+    class Program {
+        static void Main(string[] args) {
+            int myInt = 45643;
+            System.Int32 mySpecialInt = 45643;
 
-         Console.WriteLine(myInt);
-         Console.WriteLine(mySpecialInt);
+            Console.WriteLine(myInt);
+            Console.WriteLine(mySpecialInt);
         }
     }
 }
+
+/* 
+45643
+45643 
+*/
